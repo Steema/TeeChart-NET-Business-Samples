067408d0749ac023fb93f775e4732b270331f311
Pep <pep@steema.com>
2021-12-27 18:15:27 +0100

    Added TeeChart NET Business Samples.

---
+++ WinForms/StandardSeriesDemo/Properties/Resources.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Reflection;
+
+namespace StandardSeriesDemo.Properties
+{
+    internal class Resources
+    {
+        public static Image up { 
+            get {
+                return new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("StandardSeriesDemo.Resources.up.png"));
+            }  
+        }
+        public static Image down
+        {
+            get
+            {
+                return new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("StandardSeriesDemo.Resources.down.png"));
+            }
+        }
+        public static Image TeeChartWhite190x44
+        {
+            get
+            {
+                return new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("StandardSeriesDemo.Resources.TeeChartWhite190x44.png"));
+            }
+        }
+    }
+}
