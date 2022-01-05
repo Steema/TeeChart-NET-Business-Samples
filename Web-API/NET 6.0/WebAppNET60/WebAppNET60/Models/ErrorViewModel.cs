1cce2c8d747faa7adee7d9242637b1f5a419cd60
Chris Ireland <chris@steema.com>
2022-01-05 12:03:23 +0100

    New WebAppNET60 example

---
+++ Web-API/NET 6.0/WebAppNET60/WebAppNET60/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppNET60.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
